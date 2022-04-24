7aeb1acf7d6b9a6c65408517a918bd35776e9c72
ecegulden <99769370+ecegulden@users.noreply.github.com>
2022-04-24 11:21:57 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,36 @@
-from flask import render_template
+from flask import render_template, redirect, flash
 from app import myapp
+from flask_login import login_user, logout_user
 
 @myapp.route('/')
+@myapp.route('/home')
 def home():
     return render_template('home.html')
 
+# @app.route('/sellerspage')
+# def seller_page():
+# items= Item.query.filter_by( username= form.username.data).first()
+# return render_template('sellerspage.html', items=items)
+
+
 @myapp.route('/login', methods= ['POST','GET'])
 def login():
-    if( request.method== 'POST'):
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username == user['username'] and password == user['password']:
-            
-            session['user']=username
-            return redirect('/') #fill this out
-        return redirect('/login')
-    return render_template("login.html)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password_correction( password= form.password.data):
+            login_user(user)
+            flash(f'Successful Login!', category= 'success')
+            return redirect('/') #fill this out later
+        else:
+            flash('Incorrect username or password! Please try again.', category='fail')
+    return render_template("login.html", form=form)         
+             
    
 @myapp.route('/logout')
 def logout():
-    session.pop('user')
+    logout_user()
+    flash("You have been logged out.", category= 'info')
     return redirect('/login')                       
          
                           
