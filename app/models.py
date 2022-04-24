ed9ec5819542626a0ce3d58434fe11c980f60758
pranv11 <87796245+pranv11@users.noreply.github.com>
2022-04-23 18:16:06 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from app import db
 
 
-class user(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, unique = True)
     username = db.Column(db.String(length = 64), primary_key = True, unique =True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.Integer, primary_key = True, unique = True)
 
-class item(db.Model):
+class Item(db.Model):
     id = db.Column(db.Integer(), primary_key =True)
     name = db.Column(db.String(length = 32), nullable = False, unique = True)
     price = db.Column(db.Integer, nullable = False)
