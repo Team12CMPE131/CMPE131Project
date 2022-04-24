400fa611d1a83864b62662864cf72736fcbfaabe
ecegulden <99769370+ecegulden@users.noreply.github.com>
2022-04-24 10:41:44 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,25 @@ from app import myapp
 def home():
     return render_template('home.html')
 
+@myapp.route('/login', methods= ['POST','GET'])
+def login():
+    if( request.method== 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == user['username'] and password == user['password']:
+            
+            session['user']=username
+            return redirect('/') #fill this out
+        return redirect('/login')
+    return render_template("login.html)
+   
+@myapp.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/login')                       
+         
+                          
+
 @myapp.route('/list')
 def list():
     return render_template('list.html')
