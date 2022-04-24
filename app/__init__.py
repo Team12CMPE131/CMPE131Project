417960fd0ce6adfa81395f5d3f986ef5e1c01eda
pranv11 <pranavpandey2720@gmail.com>
2022-04-23 18:07:44 -0700

    added models.py file, editted init.py and added market.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 myapp = Flask(__name__)
+myapp.config.from_mapping(
+    SECRET_KEY = 'CMPE131group12',
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///market.db'
+    )
 
-from app import routes
+
+
+db = SQLAlchemy(myapp)
+
+
+from app import routes, models
